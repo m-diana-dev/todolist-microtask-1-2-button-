5fbe1a1ae669902fe7221fe9016435b674431ce3
m-diana-dev <m.diana.dev@gmail.com>
2023-04-26 22:48:34 +0300

    add 3 microtask

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
 import React, {MouseEvent} from 'react';
+import {Button} from "./components/Button";
 
 function App() {
-    // const MyFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //     console.log('I\'am Vasya');
-    // }
-    // const MySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
-    //     console.log('I\'am Petya');
-    // }
-    // const OnClickHandler = (name: string) => {
-    //     console.log(name);
-    // }
-
-    const foo1 = () => {
-        //ничего не передавать, 100200 в консоли
-        console.log('100200');
-    }
-    const foo2 = (number: number) => {
-        //передать 100200
-        console.log(number);
-    }
     return (
        <div>
-           {/*<button onClick={(event)=>{console.log('1')}}>MyYoutubeChanel-1</button>*/}
-           {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => OnClickHandler('Vasya')}>MyYoutubeChanel-1</button>*/}
-           {/*<button onClick={(event: MouseEvent<HTMLButtonElement>) => OnClickHandler('Ivan')}>MyYoutubeChanel-1</button>*/}
-
-           <button onClick={foo1}>1</button>
-           <button onClick={(event) =>foo2(100200)}>2</button>
+           <Button/>
+           <button>MyYoutubeChanel-2</button>
        </div>
     );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Button = () => {
+    return(
+        <button>MyYoutubeChanel-1</button>
+    )
+}
