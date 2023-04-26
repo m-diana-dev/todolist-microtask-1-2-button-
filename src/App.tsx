e3fa240ce006f31f34598dd171b15fb641570bb7
m-diana-dev <m.diana.dev@gmail.com>
2023-04-26 16:12:37 +0300

    add 1 microtask

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React, {useState} from 'react';
 
 function App() {
+    const MyFirstSubscriber = () => {
+        console.log('I\'am Vasya');
+    }
+    const MySecondSubscriber = () => {
+        console.log('I\'am Petya');
+    }
     return (
-       <div></div>
+       <div>
+           {/*<button onClick={(event)=>{console.log('1')}}>MyYoutubeChanel-1</button>*/}
+           <button onClick={MyFirstSubscriber}>MyYoutubeChanel-1</button>
+           <button onClick={MySecondSubscriber}>MyYoutubeChanel-1</button>
+       </div>
     );
 }
 
